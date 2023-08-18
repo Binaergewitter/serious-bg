ea4d39628cc17409109df0596bbaf98572555989
fliiiix <hi@l33t.name>
2023-08-18 07:12:01 +0200

    Allow to render html from markdown

--- serious/lib/serious/commonmarker_formater.rb
+++ serious/lib/serious/commonmarker_formater.rb
@@ -3,6 +3,6 @@ require 'commonmarker'
 
 class StupidFormatter::CommonMarker < StupidFormatter::AbstractFormatter
   def result
-    Commonmarker.to_html(input)
+    Commonmarker.to_html(input, options:{render: {unsafe:true}})
   end
 end
