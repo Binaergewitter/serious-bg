bf8b6a909bd0bca8822e380308588d086ad7970a
fliiiix <hi@l33t.name>
2023-08-28 20:28:53 +0200

    allow to render script tags as raw html

--- serious/lib/serious/commonmarker_formater.rb
+++ serious/lib/serious/commonmarker_formater.rb
@@ -3,6 +3,6 @@ require 'commonmarker'
 
 class StupidFormatter::CommonMarker < StupidFormatter::AbstractFormatter
   def result
-    Commonmarker.to_html(input, options:{render: {unsafe:true}})
+    Commonmarker.to_html(input, options:{ extension: { tagfilter: false }, render: {unsafe:true}})
   end
 end
